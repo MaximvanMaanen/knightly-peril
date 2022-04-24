6cb4ba6e2f14ec90b98718150bda5ff1f105a2ac
MaximvanMaanen <maxim.vanmaanen@gmail.com>
2022-04-24 17:06:20 +0200

    [SO-23] Added: IEnemy Interface

---
+++ src/main/java/com/github/hanyaeger/knightlyperil/entities/characters/enemy/IEnemy.java
@@ -0,0 +1,7 @@
+package com.github.hanyaeger.knightlyperil.entities.characters.enemy;
+
+/**
+ * An IEnemy represents an Enemy. This interface is used to distinguish between types of colliders.
+ */
+public interface IEnemy {
+}
